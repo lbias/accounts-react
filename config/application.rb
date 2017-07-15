207be8d3bd5c4950796755f89e27bbb775746624
lbias <306508354@qq.com>
2017-07-15 23:15:08 +0800

    include react.addons, refactor components#records addRecord and deleteRecord methods

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,8 @@ module AccountsReact
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.active_record.raise_in_transactional_callbacks = true
+
+    config.react.addons = true    
   end
 end
